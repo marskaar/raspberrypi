df8c8640ba6904f160f69757c4e39f4e92d812a7
marskaar <46196020+marskaar@users.noreply.github.com>
2018-12-27 15:45:44 +0100

    Add files via upload

---
+++ 01_led.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 11 #pin11
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.Setup(LedPin, GPIO.OUT)
+    GPIO.output(LedPin, GPIO.HIGH)
+
+def Loop():
+    while True:
+        print '..led on'
+        GPIO.output(LedPin, GPIO.LOW)
+        time.sleep(0.5)
+        print 'led off..'
+        GPIO.output(LedPin, GPIO.HIGH)
+        time.sleep(0.5)
+
+def destroy():
+    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
+        
